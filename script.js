98011f74ee959765469fea9e62c6bb122398c8ca
Grace <gracek2705@tuckahoeschools.org>
2024-11-25 01:26:21 +0000

    commit

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ document.getElementById("scissors-button").addEventListener("click", () =>{
     document.getElementById("player-choice").innerText = "Player's choice: Scissors";
 });
 
+document.getElementById("rock-button").addEventListener("click", () => {
+    document.getElementById("player-choice").innerHTML = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYKDxVCDnbHAalbHXiwbPD5lP6qTLl8JHOtg&s" alt="Rock">';
+});
+
+document.getElementById("paper-button").addEventListener("click", () =>{
+    document.getElementById("player-choice").innerHTML = '<img src="https://as1.ftcdn.net/v2/jpg/01/30/82/92/1000_F_130829242_7RUj2Q4CLzvLZEnKIlvVZNGO9XbSUjES.jpg" alt="Paper";
+});
+
 
 let playerChoice;
 document.getElementById("rock-button").addEventListener("click", () =>{
